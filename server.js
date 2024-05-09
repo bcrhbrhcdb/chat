70688211f84a2ffb69f3c33fef8338d29c8aa5c6
bcrhbrhcdb <apet2804@mpsedu.org>
2024-05-09 17:04:00 -0500

    a

--- server.js
+++ server.js
@@ -1,50 +1,99 @@
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-const adapter = new FileSync('db.json');
-const db = low(adapter);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
+const fs = require('fs');
 
-// Set some defaults (required if your JSON file is empty)
-db.defaults({ messages: [], userCooldowns: {} }).write();
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+// Load data from the JSON file
+const dataFile = 'data.json';
+let data = {};
+try {
+  data = JSON.parse(fs.readFileSync(dataFile));
+} catch (err) {
+  console.log('Error reading data file:', err);
+}
+
+// Initialize data if it doesn't exist
+if (!data.messages) data.messages = [];
+if (!data.users) data.users = {};
+
+// Save data to the JSON file
+function saveData() {
+  fs.writeFileSync(dataFile, JSON.stringify(data));
+}
 
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  socket.on('join', (name) => {
-    socket.userName = name;
-    console.log(`${name} joined the chat`);
-    io.emit('userJoined', name);
-  });
+  // Send the current chat messages to the new user
+  socket.emit('initialMessages', data.messages);
 
-  socket.on('message', (message) => {
-    const userName = socket.userName;
-    const cooldownDuration = 5000; // 5 seconds
-
-    const userCooldown = db.get('userCooldowns').find({ user: userName }).value();
-
-    if (userCooldown && Date.now() - userCooldown.lastMessageTime < cooldownDuration) {
-      socket.emit('cooldownMessage', `Please wait ${Math.ceil((cooldownDuration - (Date.now() - userCooldown.lastMessageTime)) / 1000)} seconds before sending another message.`);
-    } else {
-      db.get('messages').push({ user: userName, message }).write();
-      io.emit('newMessage', { user: userName, message });
-
-      db.get('userCooldowns').remove({ user: userName }).write();
-      db.get('userCooldowns').push({ user: userName, lastMessageTime: Date.now() }).write();
+  // Handle user joining the chat
+  socket.on('join', (username) => {
+    const existingUser = Object.values(data.users).find(user => user.username === username);
+    if (existingUser) {
+      socket.emit('error', 'Username already taken');
+      return;
     }
+
+    const user = { id: socket.id, username };
+    data.users[socket.id] = user;
+    socket.broadcast.emit('userJoined', user.username);
+    socket.emit('welcome', `Welcome to the chat, ${user.username}!`);
+    saveData();
   });
 
+  // Handle user renaming
+  socket.on('rename', (newUsername) => {
+    const user = data.users[socket.id];
+    if (!user) {
+      socket.emit('error', 'You must join the chat first');
+      return;
+    }
+
+    const existingUser = Object.values(data.users).find(u => u.username === newUsername);
+    if (existingUser) {
+      socket.emit('error', 'Username already taken');
+      return;
+    }
+
+    const oldUsername = user.username;
+    user.username = newUsername;
+    socket.broadcast.emit('userRenamed', { oldUsername, newUsername });
+    socket.emit('success', `Your username has been changed to ${newUsername}`);
+    saveData();
+  });
+
+  // Handle new message
+  socket.on('message', (message) => {
+    const user = data.users[socket.id];
+    if (!user) {
+      socket.emit('error', 'You must join the chat first');
+      return;
+    }
+
+    const messageData = { user: user.username, message };
+    data.messages.push(messageData);
+    io.emit('newMessage', messageData);
+    saveData();
+  });
+
+  // Handle user leaving the chat
   socket.on('disconnect', () => {
-    console.log('A user disconnected');
+    const user = data.users[socket.id];
+    if (user) {
+      socket.broadcast.emit('userLeft', user.username);
+      delete data.users[socket.id];
+      saveData();
+    }
   });
 });
 
-server.listen(3000, () => {
-  console.log('Server started on port 3000');
+const port = process.env.PORT || 3000;
+http.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
